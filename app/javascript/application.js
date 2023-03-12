db428c549174424cb1e9f4ddcee89097f95c9ceb
Fabrice Tuilliere <fabrice.tui@gmail.com>
2023-03-12 12:28:59 +0100

    init commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+import { showMenu } from "./components/side_menu_show";
+
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.session.drive = false;
+
+// show little screen side menu on bars icon click
+showMenu()
---
+++ app/javascript/components/side_menu_show.js
@@ -0,0 +1,19 @@
+export const showMenu = () => {
+  const navTrigger = document.querySelector(".nav-trigger");
+  const body = document.querySelector("body");
+
+  if (navTrigger) {
+    navTrigger.addEventListener("click", toggleNavigation);
+
+    function toggleNavigation(event) {
+      event.preventDefault();
+      body.classList.toggle("nav-open");
+    }
+
+    const overlay = document.querySelector(".overlay");
+    overlay.addEventListener("click", () => {
+      body.classList.remove("nav-open");
+    });
+  }
+};
+
