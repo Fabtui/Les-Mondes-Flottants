7f1c6e9fe08c6fbc561ea0e2bd78fbfa046c8fbf
Fabrice Tuilliere <fabrice.tui@gmail.com>
2023-04-17 18:02:37 +0200

    comment user mail sender

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  after_create :send_welcome_mail
+  # after_create :send_welcome_mail
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
